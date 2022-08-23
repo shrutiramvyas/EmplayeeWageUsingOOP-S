7f32045fe85cee00bf62fd061f68b495ca3dafea
Shruti <sdnk1993@gmail.com>
2022-08-23 16:06:01 +0530

    welcome in oops empwages

---
+++ ClassEmplyee.java
@@ -0,0 +1,11 @@
+class ClassEmployee {
+    public void display(){
+        System.out.println("Hello World");
+    }
+}
+class Main{
+    public static void main(String[] args) {
+        ClassEmployee emp =new ClassEmployee();
+        emp.display();
+    }
+}
