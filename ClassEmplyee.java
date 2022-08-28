abdac80b54c2cec19898888a512b1601e5a6a26b
Shruti <sdnk1993@gmail.com>
2022-08-28 22:42:39 +0530

    parttime added commit

--- ClassEmplyee.java
+++ ClassEmplyee.java
@@ -1,27 +1,33 @@
 class ClassEmployee {
-    public int fullTime = 1;
-    public int total_wage_hour = 0;
-    int wage_hour=0;
-    int working_hour=0;
-    public int isPresent = (int) (Math.random() * 10 % 2);
-
-    ClassEmployee(int wage_per_hour, int working_hour) {
-        this.wage_hour = wage_per_hour;
-        this.working_hour = working_hour;
+    public int full_time=2;
+    public int part_time=1;
+    public int emp_rate_per_hour;
+    public int emphrs=0;
+    public int empwage=0;
+    ClassEmployee(int wage_per_hour){
+        this.emp_rate_per_hour=wage_per_hour;
     }
-
-    public int calc() {
-        if (isPresent == 1) {
-            total_wage_hour = this.working_hour * this.wage_hour;
+    public int calc(){
+        double empcheck =Math.floor(Math.random()*10)%3;
+        if(empcheck==part_time) {
+            emphrs = 4;
         }
-        return total_wage_hour;
+        else if(empcheck==full_time) {
+            emphrs = 8;
+        }
+        else{
+            emphrs=0;
+        }
+        empwage=emphrs*emp_rate_per_hour;
+        return empwage;
     }
+
+
 }
 class Main{
     public static void main(String[] args) {
         int wage_per_hour=20;
-        int working_hour=8;
-        ClassEmployee emp =new ClassEmployee(wage_per_hour,working_hour);
+        ClassEmployee emp =new ClassEmployee(wage_per_hour);
         System.out.println("Total wages of employee is: " + emp.calc());
 
     }
