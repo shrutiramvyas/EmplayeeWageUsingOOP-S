12b37340f595ec1ed44c1b91acd52aee8a77ce4a
Shruti <sdnk1993@gmail.com>
2022-08-28 22:27:18 +0530

    third commit

--- ClassEmplyee.java
+++ ClassEmplyee.java
@@ -1,16 +1,28 @@
 class ClassEmployee {
-    public int fullTime=1;
-    public int empcheck = (int)(Math.random()*10%2);
-    public String calc(){
-        if ( empcheck == fullTime )
-            return "Employee is Present";
-        else
-            return "Employee is Absent";
+    public int fullTime = 1;
+    public int total_wage_hour = 0;
+    int wage_hour=0;
+    int working_hour=0;
+    public int isPresent = (int) (Math.random() * 10 % 2);
+
+    ClassEmployee(int wage_per_hour, int working_hour) {
+        this.wage_hour = wage_per_hour;
+        this.working_hour = working_hour;
+    }
+
+    public int calc() {
+        if (isPresent == 1) {
+            total_wage_hour = this.working_hour * this.wage_hour;
+        }
+        return total_wage_hour;
     }
 }
 class Main{
     public static void main(String[] args) {
-        ClassEmployee emp =new ClassEmployee();
-        System.out.println(emp.calc());
+        int wage_per_hour=20;
+        int working_hour=8;
+        ClassEmployee emp =new ClassEmployee(wage_per_hour,working_hour);
+        System.out.println("Total wages of employee is: " + emp.calc());
+
     }
 }
